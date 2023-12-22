19338401c0c13a5065e25ffeaab772fe817c630d
NhatPhuong2106 <phuongily24@gmail.com>
2023-12-22 14:13:30 +0700

    feat: EDA

--- Source/Visualize/visualize_frequency_words.py
+++ Source/Visualize/visualize_frequency_words.py
@@ -1,9 +1,9 @@
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-def visualize_frequency_words(news_df, title):
+def visualize_frequency_words(news_df, col_name, title = ''):
     words_df = [] 
-    news_df['content'].apply(lambda x: words_df.extend(x))
+    news_df[col_name].apply(lambda x: words_df.extend(x))
     wordcloud = WordCloud(background_color="white").generate(' '.join(words_df))
     plt.imshow(wordcloud)
     plt.axis("off")
